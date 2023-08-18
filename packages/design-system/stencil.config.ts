f1eaad44075d57c4683313b904737648a79986c8
Hugo <albrechthugo4@gmail.com>
2023-08-18 07:15:40 -0300

    chore: adjust stencil.config customElements auto define

--- packages/design-system/stencil.config.ts
+++ packages/design-system/stencil.config.ts
@@ -1,6 +1,6 @@
-import { Config } from '@stencil/core';
+import { Config } from '@stencil/core'
 
-import { sass } from '@stencil/sass';
+import { sass } from '@stencil/sass'
 
 export const config: Config = {
   namespace: 'design-system',
@@ -8,32 +8,32 @@ export const config: Config = {
   sourceMap: true,
 
   extras: {
-    experimentalImportInjection: true,
+    experimentalImportInjection: true
   },
   plugins: [sass()],
   outputTargets: [
     {
       type: 'dist',
-      esmLoaderPath: '../loader',
+      esmLoaderPath: '../loader'
     },
     {
-      type: 'dist-custom-elements',
+      type: 'dist-custom-elements'
     },
     {
-      type: 'docs-readme',
+      type: 'docs-readme'
     },
     {
       type: 'www',
-      serviceWorker: null, // disable service workers
+      serviceWorker: null // disable service workers
     },
     {
       type: 'dist-hydrate-script',
-      dir: 'dist/hydrate',
+      dir: 'dist/hydrate'
     },
     {
       type: 'dist-custom-elements',
-      autoDefineCustomElements: true,
-      includeGlobalScripts: false,
-    },
-  ],
-};
+      customElementsExportBehavior: 'auto-define-custom-elements',
+      includeGlobalScripts: false
+    }
+  ]
+}
